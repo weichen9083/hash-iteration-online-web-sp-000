dc8adb89a3d5c1c5560d51aa27c6dff4d4b26b7a
wei chen <weichen9083@gmail.com>
2019-09-06 02:53:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,13 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age > 9 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+    end 
+  
+end 
